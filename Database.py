92af365302bab15c2aaad4ce1dae6f6e9e7ef461
JuanBejarano-dev <juan.bejarano16.dev@gmail.com>
2025-05-06 16:58:45 -0500

    Agrego Bd con sqlite3 al proyecto

--- Database.py
+++ Database.py
@@ -1,1 +1,8 @@
-hola = hola 
+import sqlite3 
+
+try: 
+    conexion = sqlite3.connect("bd_Tienda")
+    cursor = conexion.cursor()
+    cursor.execute("CREATE TABLE Persona (nombre VARCHAR(50))")
+except Exception as ex:
+    print("Error al conectar a la base de datos: ", ex)
